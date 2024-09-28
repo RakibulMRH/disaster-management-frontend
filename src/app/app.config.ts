8d002e95fd419d2eae76ef3426ad052bc44d2ab8
MD. RAKIBUL HASAN <rakibulmrh4bd@gmail.com>
2024-09-28 14:41:41 +0600

    app configuration   imports and enhance module organization

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,23 +1,25 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, RouterModule } from '@angular/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NgIf } from '@angular/common';
+import { NgIf, CommonModule } from '@angular/common';
 import { appRoutes } from './app.routes';
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
-import { ToastrModule } from 'ngx-toastr';
-import { ToastrService } from 'ngx-toastr';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(appRoutes),
     provideHttpClient(withFetch()),
-    importProvidersFrom(Component, ToastrModule.forRoot(),BrowserAnimationsModule, ActivatedRoute, FormBuilder, FormGroup, Validators, FormsModule, ReactiveFormsModule, NgIf, CommonModule, RouterModule),
+    importProvidersFrom(
+      FormsModule,
+      ReactiveFormsModule,
+      NgIf,
+      CommonModule,
+      RouterModule,
+      BrowserAnimationsModule,
+      ToastrModule.forRoot()
+    ),
   ],
 };
